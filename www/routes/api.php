0b32cb361f118a44f93120c9c25ea923c0bdf00d
Andrii Pavliuk <31381969+lv051187paa@users.noreply.github.com>
2022-12-26 23:44:25 +0200

    implement jwt token

--- www/routes/api.php
+++ www/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
@@ -20,5 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostController::class);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::apiResource('posts', PostController::class)->middleware('auth:api');
 Route::apiResource('posts/{post}/comments', CommentController::class);
